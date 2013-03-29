473e2a53341ca21cd33d8d001ee4fc1e3a871fa0
Nicolas Brousse <pro@nicolas-brousse.fr>
2013-03-29 18:19:47 +0100

    Bump to 1.0.3

--- init.rb
+++ init.rb
@@ -8,7 +8,7 @@ Redmine::Plugin.register :rougemine do
   name 'Rougemine plugin'
   author 'Opsone'
   description 'This is a plugin for Rougemine mobile application'
-  version '1.0.2'
+  version '1.0.3'
   url 'http://rougemine.opsone.net'
   author_url 'http://www.opsone.net'
   requires_redmine :version_or_higher => '1.1.0'
